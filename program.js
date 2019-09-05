94948f093ffaf0f81148635936f4e862dabfb8f1
TJ Jackson <elitefourfresh@gmail.com>
2019-09-05 11:39:16 -0400

    second commit

--- program.js
+++ program.js
@@ -33,3 +33,24 @@
 //     const lines = contents.toString().split('\n').length - 1;
 //     console.log(lines);
 // });
+
+/* FILTERED LS */
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const folder = process.argv[2];
+// const extension = '.' + process.argv[3];
+
+// fs.readdir(folder, function (err, files) {
+//     if(err) throw err;
+
+//     files.forEach(function (file) {
+//         if (path.extname(file) === extension) {
+//             console.log(file);
+//         };
+//     });
+// });
+
+/* MAKE IT MODULAR */
+
